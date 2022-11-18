302f37e746282e68e1e6459442326cd6065471a8
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2022-11-17 21:43:42 -0300

    feat: configurando tema com styled components

---
+++ src/styles/global.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  *, *::after, *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body, input, textarea, button {
+    font-family: ${(props) => props.theme.font};
+    font-weight: ${(props) => props.theme.fontWeights.normal};
+    font-size: ${(props) => props.theme.fontSizes.md};
+  }
+
+  h1, h2, h3, strong {
+    color: ${(props) => props.theme.colors.black};
+  }
+
+  body {
+    color: ${(props) => props.theme.colors.gray400};
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+  font: 'Red Hat Display, sans-serif',
+  fontSizes: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    md: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem',
+    '3xl': '1.875rem',
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+  },
+  colors: {
+    black: '#000',
+    white: '#FFF',
+    gray500: '#373B53',
+    gray400: '#A6ACBE',
+    purple500: '#6F52ED',
+  },
+}
